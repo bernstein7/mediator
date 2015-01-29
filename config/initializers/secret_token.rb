5d902bdd3f3f88731876b9cef41a6ec88c8a00bb
bernstein <clapandslap@gmail.com>
2015-01-29 18:33:59 +0200

    secret token not ignored now

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,2 +1,2 @@
 Mediator::Application.config.secret_key_base = Rails.application.secrets['secret_key_base']
-Mediator::Application.config.secret_token = Rails.application.secrets['secret_token']
+Mediator::Application.config.secret_token    = Rails.application.secrets['secret_token']
